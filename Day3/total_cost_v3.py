1d3d7d0fedd4e6c534ec78465a6bff1a0de7d96d
BrokenFate007 <krishnapotter007@gmail.com>
2025-08-26 18:24:58 +0530

    when even with the 100"%" saving, is not possible

--- Day3/total_cost_v3.py
+++ Day3/total_cost_v3.py
@@ -11,24 +11,33 @@ month_target = 36
 low = 0
 high = 10000
 steps = 0
-
-while low<=high:
-    current_saving = 0
-    portion_saved_guess = int((low+high)/2)
-    temp_annual_salary = annual_salary
-    for month in range(36):
-        monthly_contribution = (temp_annual_salary/12)*(portion_saved_guess/10000)
-        current_saving +=current_saving*(r/12)+monthly_contribution
-        if (month%6 == 0) and ( month > 0):
-            temp_annual_salary+=temp_annual_salary*semi_annual_raise
-    if abs(current_saving-down_payment)<epsilon:
-        print("Best saving rate: ",portion_saved_guess/10000)
-        break
-    elif current_saving<down_payment:
-        low = portion_saved_guess
-    elif current_saving>down_payment:
-        high = portion_saved_guess
-    steps+=1
-
-print("Steps in Bisection search:",steps)
+current_saving = 0
+temp_annual_salary = annual_salary
+for month in range(36):
+    portion_saved_guess=10000
+    monthly_contribution = (temp_annual_salary/12)*(portion_saved_guess/10000)
+    current_saving +=current_saving*(r/12)+monthly_contribution
+    if (month%6 == 0) and ( month > 0):
+        temp_annual_salary+=temp_annual_salary*semi_annual_raise
+if down_payment - current_saving > epsilon:
+    print("It is not possible to pay the down payment in three years.")
+else:
+    while low<=high:
+        current_saving = 0
+        portion_saved_guess = int((low+high)/2)
+        temp_annual_salary = annual_salary
+        for month in range(36):
+            monthly_contribution = (temp_annual_salary/12)*(portion_saved_guess/10000)
+            current_saving +=current_saving*(r/12)+monthly_contribution
+            if (month%6 == 0) and ( month > 0):
+                temp_annual_salary+=temp_annual_salary*semi_annual_raise
+        if abs(current_saving-down_payment)<epsilon:
+            print("Best saving rate: ",portion_saved_guess/10000)
+            break
+        elif current_saving<down_payment:
+            low = portion_saved_guess+1
+        elif current_saving>down_payment:
+            high = portion_saved_guess+1
+        steps+=1
+    print("Steps in Bisection search:",steps)
     
