737ca18c956e542704b778d058495ff39ffbe714
BrokenFate007 <krishnapotter007@gmail.com>
2025-08-26 11:35:57 +0530

    Day1: My first Python program

---
+++ Day1/hello_world_20250823.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 23 16:15:56 2025
+
+@author: KRISHNA MURARI
+"""
+
+print("hello world")
---
+++ Day1/ps0_problem_assignment_0_20250823.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 23 18:52:27 2025
+
+@author: KRISHNA MURARI
+"""
+
+import numpy
+   
+x = int(input("Enter number x:"))
+y = int(input("Enter number y:"))
+a = x**y
+print("x**y:",a)
+l = float(numpy.log(x)/numpy.log(2))
+print("log(base2)(x):",l)
